49b62a438c846d737dbd98e660726c7b2e26f399
stevebiczyk <stevebiczyk@gmail.com>
2025-04-03 21:23:56 +0100

    dynamic rendering + generateStaticParams

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
 // This is a server component
+import { cookies } from "next/headers";
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
   console.log("About server component rendered");
   return <h1>About Page</h1>;
 }
---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,19 @@
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  console.log("Product page server component rendered");
+  return (
+    <>
+      <h1>
+        Product {id} details rendered at {new Date().toLocaleTimeString()}
+      </h1>
+    </>
+  );
+}
