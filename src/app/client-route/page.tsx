cc7ded394d979b19c0d72434d1d84dc3f5c45751
stevebiczyk <stevebiczyk@gmail.com>
2025-04-25 21:55:11 +0100

    Context Providers

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -4,34 +4,34 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
-  const settings = {
-    dots: true,
-  };
-  return (
-    <>
-      <div style={{ color: theme.colors.primary }}>
-        <h1>Client route page</h1>
-      </div>
-      <div className="image-slider-container">
-        <Slider {...settings}>
-          <div>
-            <img src="https://picsum.photos/400/200" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/200" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/200" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/200" />
-          </div>
-        </Slider>
-      </div>
-    </>
-  );
+  // const settings = {
+  //   dots: true,
+  // };
+  return;
+  <h1 style={{ color: theme.colors.secondary }}>Client router page</h1>;
+  // <>
+  //   <div style={{ color: theme.colors.primary }}>
+  //     <h1>Client route page</h1>
+  //   </div>
+  //   <div className="image-slider-container">
+  //     <Slider {...settings}>
+  //       <div>
+  //         <img src="https://picsum.photos/400/200" />
+  //       </div>
+  //       <div>
+  //         <img src="https://picsum.photos/400/200" />
+  //       </div>
+  //       <div>
+  //         <img src="https://picsum.photos/400/200" />
+  //       </div>
+  //       <div>
+  //         <img src="https://picsum.photos/400/200" />
+  //       </div>
+  //     </Slider>
+  //   </div>
+  // </>
 }
